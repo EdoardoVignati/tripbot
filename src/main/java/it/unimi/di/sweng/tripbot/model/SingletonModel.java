fd5916ee17fe2f358bef83fce7c1fdea5de8656f
AlessandroTironi <alessandro.tironi@studenti.unimi.it>
2016-06-04 15:52:31 +0200

    REFACTORING: pulizia codice

--- src/main/java/it/unimi/di/sweng/tripbot/model/SingletonModel.java
+++ src/main/java/it/unimi/di/sweng/tripbot/model/SingletonModel.java
@@ -47,11 +47,10 @@ public enum SingletonModel implements IModel {
 	@Override
 	public synchronized void removePointOfInterest(String groupId, String name) {
 		List<PointOfInterest> list = POINTS_OF_INTEREST.get(groupId);
-		if (list != null) {
+		if (list != null)
 			for (int i = 0; i < list.size(); i++) 
 				if (list.get(i).name.equals(name))
 					list.remove(i);
-		}
 	}
 	
 	public synchronized void loadMap(final Map<String, List<PointOfInterest>> newMap) {
