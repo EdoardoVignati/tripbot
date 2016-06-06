b549b5efb37239d8305ecdc50fc97c0ad4db8a9c
Stefano Savino <stefano.savino@studenti.unimi.it>
2016-06-06 16:15:57 +0200

    REFACTORING: risolti problemi con shippable e test del develop

--- src/test/java/it/unimi/di/sweng/tripbot/PointOfInterestTest.java
+++ src/test/java/it/unimi/di/sweng/tripbot/PointOfInterestTest.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -15,6 +17,7 @@ public class PointOfInterestTest {
 	@Test
 	public void pointOfInterestTest() throws ParseException {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-hh.mm.ss");
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Rome"));
 		Date date = dateFormat.parse("2016-06-03-09.00.00");
 		GMapsPosition mockPosition = Mockito.mock(GMapsPosition.class);
 		Mockito.when(mockPosition.toString()).thenReturn("fake-position");
