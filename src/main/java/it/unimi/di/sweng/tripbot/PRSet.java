57331b1ac200e970e516726c38f3c07f6a39c790
Alberto Butifar <alberto.butifar@studenti.unimi.it>
2016-06-06 22:59:24 +0200

    VERDE: testPRSet

--- src/main/java/it/unimi/di/sweng/tripbot/PRSet.java
+++ src/main/java/it/unimi/di/sweng/tripbot/PRSet.java
@@ -5,9 +5,18 @@ import com.pengrad.telegrambot.model.Message;
 public class PRSet implements IFunctionality {
 
 	@Override
-	public String exec(Message dumpMessage) {
-		// TODO Auto-generated method stub
-		return null;
+	public String exec(Message message) {
+		
+		String output = "";
+
+		String testoMessaggio = message.text();
+		
+		String[] datiMessaggio = testoMessaggio.split(" ");
+		
+		for(int i=1; i<datiMessaggio.length; i++)
+			output += " " + datiMessaggio[i];
+		
+		return "punto di ritrovo '" + output.trim() + "' impostato";
 	}
 
 }
