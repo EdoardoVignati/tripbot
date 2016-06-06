4e2360d7143954fc9711bf5e21b991b252e8df8f
Alberto Butifar <alberto.butifar@studenti.unimi.it>
2016-06-06 23:07:01 +0200

    VERDE: testStartManager

--- src/main/java/it/unimi/di/sweng/tripbot/StartManager.java
+++ src/main/java/it/unimi/di/sweng/tripbot/StartManager.java
@@ -6,7 +6,7 @@ public class StartManager implements IFunctionality {
 
 	@Override
 	public String exec(Message message) {
-		return null;
+		return "Ciao, sono TripBot, un bot che permette di organizzare e gestire viaggi di gruppo";
 	}
 
 }
