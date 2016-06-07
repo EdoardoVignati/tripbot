7d240603dca65440bb31c9d925e011d9600e58de
Federico Cao <federico.cao@studenti.unimi.it>
2016-06-07 18:07:03 +0200

    VERDE: aggiunto test esecuzione query e creazione metodi execQuery e close

--- src/main/java/it/unimi/di/sweng/tripbot/database/Database.java
+++ src/main/java/it/unimi/di/sweng/tripbot/database/Database.java
@@ -2,14 +2,37 @@ package it.unimi.di.sweng.tripbot.database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
 	
 	Connection conn;
+	Statement st;
+	ResultSet rs;
 	
 	public Database(String url) throws SQLException{
-		conn = DriverManager.getConnection(url);		
+		conn = DriverManager.getConnection(url);
+		st = null;
+		rs = null;		
 	}	
+	
+	public ResultSet execQuery(String query) throws SQLException {
+		st = conn.createStatement();
+
+		if (query.toUpperCase().contains("SELECT"))
+			rs = st.executeQuery(query);
+		else
+			st.executeUpdate(query);
+
+		return rs;
+	}
+
+	public void closeConnection() throws SQLException {
+		rs.close();
+		st.close();
+		conn.close();
+	}
 	
 }
--- src/test/java/it/unimi/di/sweng/tripbot/database/DatabaseTest.java
+++ src/test/java/it/unimi/di/sweng/tripbot/database/DatabaseTest.java
@@ -1,7 +1,8 @@
 package it.unimi.di.sweng.tripbot.database;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import org.junit.BeforeClass;
@@ -11,24 +12,40 @@ import org.junit.rules.Timeout;
 
 public class DatabaseTest {
 	private static String url;
-	
+
 	@BeforeClass
-	public static void init(){
+	public static void init() {
 		url = System.getenv("JDBC_DATABASE_URL");
 	}
-	
+
 	@Rule
 	public Timeout globalTimeout = Timeout.seconds(10);
 
 	@Test
 	public void getConnectionOk() throws SQLException {
-			Database db = new Database(url);
-			assertTrue(db.conn.isValid(10));
+		Database db = new Database(url);
+		assertTrue(db.conn.isValid(10));
 	}
 
 	@Test(expected = SQLException.class)
 	public void getConnectionNo() throws SQLException {
 		new Database("");
+	}
+
+	@Test
+	public void execQuery() throws SQLException {
+		Database db = new Database(url);
+		db.execQuery("CREATE TABLE test (poi varchar(20));");
+		db.execQuery("INSERT INTO test (poi) VALUES ('Duomo di Milano');");
+
+		ResultSet poiElem = db.execQuery("SELECT poi FROM test;");
+		poiElem.next();
+		assertEquals("Duomo di Milano", poiElem.getString(1));
+
+		db.execQuery("DROP table test");
+
+		poiElem.close();
+		db.closeConnection();
 
 	}
 
