12043fc53587a963838f795051af89a84f5e46eb
Alberto Butifar <alberto.butifar@studenti.unimi.it>
2016-06-07 21:28:29 +0200

    REFACTORING: estratte costanti e variabili + riorganizzato codice

--- src/main/java/it/unimi/di/sweng/tripbot/PRSet.java
+++ src/main/java/it/unimi/di/sweng/tripbot/PRSet.java
@@ -18,37 +18,45 @@ public class PRSet implements IFunctionality {
 	public String exec(Message message) {
 		
 		final String pattern = "(\\/set_punto_ritrovo) (.+) (\\d\\d\\/\\d\\d\\/\\d\\d\\d\\d) (\\d\\d:\\d\\d)";
+		
+		final int INDICE_GRUPPO_LUOGO = 2;
+		final int INDICE_GRUPPO_DATA = 3;
+		final int INDICE_GRUPPO_ORA = 4;
 
 		final String testoMessaggio = message.text();
 		
-	    Pattern myPattern = Pattern.compile(pattern);
+	    final Pattern myPattern = Pattern.compile(pattern);
 
-	    Matcher myMatcher = myPattern.matcher(testoMessaggio);
+	    final Matcher myRegExpMatcher = myPattern.matcher(testoMessaggio);
 	    
-	    if ( !myMatcher.find() )
+	    if ( !myRegExpMatcher.find() )
 	    	return "formato input non corretto";
 		
-		final String luogo = myMatcher.group(2);
-		final String data = myMatcher.group(3);
-		final String ora = myMatcher.group(4);
+		final String luogo = myRegExpMatcher.group(INDICE_GRUPPO_LUOGO);
+		final String data = myRegExpMatcher.group(INDICE_GRUPPO_DATA);
+		final String ora = myRegExpMatcher.group(INDICE_GRUPPO_ORA);
+		
+		final String groupID = message.chat().id().toString();
 		
 		try {
 			
 			final SimpleDateFormat formatterData = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 			final Date dataPR = formatterData.parse(data + " " + ora);
-			final String groupID = message.chat().id().toString();
+
 			final ILocationService myLocationProvider = new LocationProvider();
 			final APosition gmapsPosition = myLocationProvider.getPositionByName(luogo);
 			
 			final PointOfInterest newPR = new PointOfInterest(luogo, dataPR, gmapsPosition, groupID);
 			
-			IModel model = SingletonModel.INSTANCE;
-			model.insertNewPointOfInterest(newPR);
+			final IModel myModel = SingletonModel.INSTANCE;
+			myModel.insertNewPointOfInterest(newPR);
 			
 			return "punto di ritrovo '" + luogo + " " + formatterData.format(dataPR) + "' impostato";
 			
 		} catch (Exception e) {
+			
 			return "luogo non trovato su google maps";
+			
 		}
 		
 	}
