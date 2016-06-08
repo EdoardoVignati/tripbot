eef94222016436bf736cf7507244f01c4886a3a8
Alberto Butifar <alberto.butifar@studenti.unimi.it>
2016-06-08 22:01:56 +0200

    VERDE: testPRGetNext() e creato comparator per i punti di interesse

--- src/main/java/it/unimi/di/sweng/tripbot/PRNext.java
+++ src/main/java/it/unimi/di/sweng/tripbot/PRNext.java
@@ -1,5 +1,8 @@
 package it.unimi.di.sweng.tripbot;
 
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.regex.Matcher;
@@ -31,13 +34,26 @@ public class PRNext implements IFunctionality {
 			
 			final List<PointOfInterest> myPRList = SingletonModel.INSTANCE.getPointOfInterestList(groupID);
 			
+			final PointOfInterestComparator myComparator = new PointOfInterestDateComparator();
+			
+			Collections.sort(myPRList, myComparator);
+			
+			final PointOfInterest currentPRDate = new PointOfInterest(null, new Date(), null, null);
+			
+			final SimpleDateFormat formatterData = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+			
+			for(PointOfInterest tmp : myPRList)
+				if( myComparator.compare(tmp, currentPRDate) >= 0 )
+					return "il prossimo punto di ritrovo e': " + tmp.name + " " + formatterData.format(tmp.meetDate);
+			
+			
 		} catch (NoSuchElementException e) {
 			
 			return "non e' stato impostato alcun punto di ritrovo";
 			
 		}
 		
-		return "il prossimo punto di ritrovo e': luogo data HH:MM";
+		return "non sono in programma altri punti di ritrovo";
 	}
 
 }
